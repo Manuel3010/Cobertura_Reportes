6ef5517a4c34f9cac3bca754690dcc7b05ac1967
manuel <manuel@Manuel>
2017-06-28 13:45:25 -0600

    28-junio-13:44 -- ManagedBeans agregadas

---
+++ registrosMvnApp-web/src/main/java/registros/tpi/Backing/FrmEstudiante.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registros.tpi.Backing;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author manuel
+ */
+@Named(value = "frmEstudiante")
+@ViewScoped
+public class FrmEstudiante {
+
+    /**
+     * Creates a new instance of FrmEstudiante
+     */
+    public FrmEstudiante() {
+    }
+    
+}
