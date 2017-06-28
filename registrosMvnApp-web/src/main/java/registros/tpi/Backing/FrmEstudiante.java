c872706462c1c333bdc5b2cd76f9f3e2549f9b6c
manuel <manuel@Manuel>
2017-06-28 14:46:44 -0600

    28-junio-14:46 -- ManagedBeans con LazyDataModel

--- registrosMvnApp-web/src/main/java/registros/tpi/Backing/FrmEstudiante.java
+++ registrosMvnApp-web/src/main/java/registros/tpi/Backing/FrmEstudiante.java
@@ -5,8 +5,18 @@
  */
 package registros.tpi.Backing;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+import registros.tpi.Sessions.EstudianteFacadeLocal;
+import registros.tpi.registrolibreria.Estudiante;
 
 /**
  *
@@ -14,12 +24,87 @@ import javax.faces.view.ViewScoped;
  */
 @Named(value = "frmEstudiante")
 @ViewScoped
-public class FrmEstudiante {
+public class FrmEstudiante implements Serializable{
 
+   private LazyDataModel<Estudiante> modelo;
+   private Estudiante registroEstudiante;
+   
+   @EJB
+   private EstudianteFacadeLocal estudianteFacade;
+       
     /**
      * Creates a new instance of FrmEstudiante
      */
     public FrmEstudiante() {
+    }
+    
+     @PostConstruct
+    public void init(){
+        
+         setModelo(new LazyDataModel<Estudiante>(){
+
+            @Override
+            public List<Estudiante> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+                List salida = new ArrayList();
+                if(estudianteFacade != null){
+                    this.setRowCount(estudianteFacade.count());
+                    int[] rango = new int[2];
+                    rango[0] = first;
+                    rango[1] = pageSize;
+                    salida = estudianteFacade.findRange(rango);
+                }
+                return salida;
+            }
+
+            @Override
+            public Object getRowKey(Estudiante object) {
+                return object.getCarnet();
+            }
+
+            @Override
+            public Estudiante getRowData(String rowKey) {
+                if(this.getWrappedData()!=null){
+                    List<Estudiante> lista = (List<Estudiante>) this.getWrappedData();
+                    if(!lista.isEmpty()) {
+                        for(Estudiante get : lista) {
+                            if(get.getCarnet().compareTo((rowKey))==0) {
+                                return get;
+                            }
+                        }
+                    }
+                }
+                return null;
+            }       
+        });
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    public LazyDataModel<Estudiante> getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(LazyDataModel<Estudiante> modelo) {
+        this.modelo = modelo;
+    }
+
+    public Estudiante getRegistroEstudiante() {
+        return registroEstudiante;
+    }
+
+    public void setRegistroEstudiante(Estudiante registroEstudiante) {
+        this.registroEstudiante = registroEstudiante;
     }
     
 }
--- registrosMvnApp-web/src/main/java/registros/tpi/Backing/FrmPersona.java
+++ registrosMvnApp-web/src/main/java/registros/tpi/Backing/FrmPersona.java
@@ -5,8 +5,19 @@
  */
 package registros.tpi.Backing;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+import registros.tpi.Sessions.PersonaFacadeLocal;
+
+import registros.tpi.registrolibreria.Persona;
 
 /**
  *
@@ -14,12 +25,82 @@ import javax.faces.view.ViewScoped;
  */
 @Named(value = "frmPersona")
 @ViewScoped
-public class FrmPersona {
+public class FrmPersona implements Serializable {
 
+    private LazyDataModel<Persona> modelo;
+    
+    @EJB
+    private PersonaFacadeLocal personaFacade;
+    
+    private Persona registroPersona;
+   
     /**
      * Creates a new instance of FrmPersona
      */
     public FrmPersona() {
     }
+    
+    @PostConstruct
+    public void init(){
+        
+         setModelo(new LazyDataModel<Persona>(){
+
+            @Override
+            public List<Persona> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+                List salida = new ArrayList();
+                if(personaFacade != null){
+                    this.setRowCount(personaFacade.count());
+                    int[] rango = new int[2];
+                    rango[0] = first;
+                    rango[1] = pageSize;
+                    salida = personaFacade.findRange(rango);
+                }
+                return salida;
+            }
+
+            @Override
+            public Object getRowKey(Persona object) {
+                return object.getDui();
+            }
+
+            @Override
+            public Persona getRowData(String rowKey) {
+                if(this.getWrappedData()!=null){
+                    List<Persona> lista = (List<Persona>) this.getWrappedData();
+                    if(!lista.isEmpty()) {
+                        for(Persona get : lista) {
+                            if(get.getDui().compareTo(Integer.parseInt(rowKey))==0) {
+                                return get;
+                            }
+                        }
+                    }
+                }
+                return null;
+            }       
+        });
+    
+    
+    
+    }
+
+    public LazyDataModel<Persona> getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(LazyDataModel<Persona> modelo) {
+        this.modelo = modelo;
+    }
+
+    public Persona getRegistroPersona() {
+        return registroPersona;
+    }
+
+    public void setRegistroPersona(Persona registroPersona) {
+        this.registroPersona = registroPersona;
+    }
+    
+    
+    
+    
     
 }
